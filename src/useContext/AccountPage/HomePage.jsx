e8f8195e755446a08deb23eb3da037973f0a6d11
steve <sofacles@yahoo.com>
2022-03-26 09:04:22 -0700

    add comments

--- src/useContext/AccountPage/HomePage.jsx
+++ src/useContext/AccountPage/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { AccountPage } from "./AccountPage";
 import { BankAccountProvider } from "./BankAccountContext";
 
@@ -7,18 +7,17 @@ import { BankAccountProvider } from "./BankAccountContext";
  * BankService.  Let's have BankAccount provide us this stuff and in this component,
  * we call setState on BankAccountContext.  For some reason I want to simulate initializing
  * the context from the home page. 
- *
 
- * I was going to use Avatar to demonstrate that you can share some state in a context.  
-   I think I wanted to show that if you had some dummy
- * service that could return account info from the login method and empty account info
- * from the logout method, the provider provide everything you need to show/hide Account Balance
+ * I was goint to use Avatar to demonstrate that you can share some state in a context.  
+ * BankService is a dummy service that returns account info from the login
+ * method and empty account info
+ * from the logout method, the provider provides everything you need to show/hide Account Balance
  * and toggle the "login"/"logout" text on the button inside <Avatar />.
- * So, have avatar tell BankAccountContext when to log in or log out.
- * BankAccountContext also knows whether it's logged in or not, so it exposes that in its 
+ * So, have avatar tell AccountPage when to set the logged in/out state on BankAccountContext.
+ * BankAccountContext knows whether it's logged in or not and exposes that in its 
  * exported state.
- *
  */
+
 const HomePage = () => {
   return (
     <BankAccountProvider>
