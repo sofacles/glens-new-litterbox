e2bd28b67311bc09f89b337ec322c8eaab6421b6
Steve Degermark <sofacles@yahoo.com>
2022-12-26 10:27:14 -0800

    renaming stuff in test

--- src/pretender/hooks/test-helpers/SimpleConsumerOfOffsetMountainDataProvider.tsx
+++ src/pretender/hooks/test-helpers/SimpleConsumerOfOffsetMountainDataProvider.tsx
@@ -7,17 +7,28 @@ import {
 
 const ComponentThatUsesHook = () => {
     const { state, dispatch } = useContext(OffsetMountainDataContext);
-const moveShipRight = () => {
+    const moveMountainsRight = () => {
+        dispatch({type: 'UPDATE_GAME_OFFSET', cargo: {offsetDifference: 10}})
+    }
+    const moveMountainsLeft = () => {
+        dispatch({type: 'UPDATE_GAME_OFFSET', cargo: {offsetDifference: -10}})
+    }
+
+    const moveMountains150Left = () => {
+        dispatch({type: 'UPDATE_GAME_OFFSET', cargo: {offsetDifference: 150}})
+    }
     
-    dispatch({type: 'UPDATE_GAME_OFFSET', cargo: {offsetDifference: -10}})
-}
     // console.info(state);
     return(
     <div>
+        <h1>test component</h1>
         <div>
-            <button data-testid="moveRightBtn" onClick={moveShipRight}>go right</button>
+            <button  onClick={moveMountainsRight}>move mountains right</button>
+            <button  onClick={moveMountainsLeft}>move mountains left</button>
+            <button  onClick={moveMountains150Left}>move mountains 150 pixels left</button>
         </div>
-        test component <span>offset: {state.gameOffset}</span>
+         <span>offset: {state.gameOffset}</span>
+         <div data-testid="leftMostPixel">LeftMostPixel is {`x: ${state.allPointsCorrected[0].x}, y:${state.allPointsCorrected[0].y}`} </div>
     </div>
     ); 
 };
--- src/pretender/hooks/useOffsetMountainData.test.jsx
+++ src/pretender/hooks/useOffsetMountainData.test.jsx
@@ -1,8 +1,8 @@
 import React, { useContext } from "react";
 import { getByTestId, fireEvent, render, screen } from "@testing-library/react";
 import { SimpleConsumerOfOffsetMountainDataProvider } from "./test-helpers/SimpleConsumerOfOffsetMountainDataProvider";
+import peaks from "../MountainData.js";
 
-console.log("The top of the test file");
 const renderTestContainer = () => {
   render(<SimpleConsumerOfOffsetMountainDataProvider />);
 };
@@ -10,11 +10,35 @@ const renderTestContainer = () => {
 describe("renders with the correct initial state", () => {
   beforeEach(() => {
     renderTestContainer();
-    screen.debug();
   });
 
   it("has zero initial offset", () => {
     const linkElement = screen.getByText(/offset: 0/i);
+    expect(linkElement).toBeInTheDocument();
+    screen.debug();
+  });
+
+  // jest has these screen dimensions by default: Height: 768,  Width: 1024
+  // useScreenDimensions will set this to will 718 x 1024 when it subtracts the height of the instrument panel
+  it("has not shuffled any of the peaks around", () => {
+    const verticalOffset = 718;
+    expect(screen.getByTestId("leftMostPixel")).toHaveTextContent(
+      `LeftMostPixel is x: ${peaks[0].x}, y:${verticalOffset}`
+    );
+  });
+});
+
+describe("when UPDATE_GAME_OFFSET is dispatched with a positive number", () => {
+  beforeEach(() => {
+    renderTestContainer();
+    //
+    for (let i = 0; i < 5; i++) {
+      fireEvent.click(screen.getByText("move mountains right"));
+    }
+  });
+
+  it("has a positive gameOffset", () => {
+    const linkElement = screen.getByText(/offset: 50/i);
     expect(linkElement).toBeInTheDocument();
   });
 });
@@ -22,12 +46,27 @@ describe("renders with the correct initial state", () => {
 describe("when UPDATE_GAME_OFFSET is dispatched with a negative number", () => {
   beforeEach(() => {
     renderTestContainer();
-    //fireEvent.click(screen.getByText("go right"));
-    fireEvent.click(screen.getByTestId("moveRightBtn"));
+    //
+    for (let i = 0; i < 5; i++) {
+      fireEvent.click(screen.getByText("move mountains left"));
+    }
   });
 
-  it("has a negative offset", () => {
-    const linkElement = screen.getByText(/offset: -10/i);
+  it("has a negative gameOffset", () => {
+    const linkElement = screen.getByText(/offset: -50/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+});
+
+describe("when UPDATE_GAME_OFFSET is dispatched with a positive number greater than PANEL_WIDTH", () => {
+  beforeEach(() => {
+    renderTestContainer();
+
+    fireEvent.click(screen.getByText("move mountains 150 pixels left"));
+  });
+
+  it("has a positive gameOffset", () => {
+    const linkElement = screen.getByText(/offset: 150/i);
     expect(linkElement).toBeInTheDocument();
   });
 });
