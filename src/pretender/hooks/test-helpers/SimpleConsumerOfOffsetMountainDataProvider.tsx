4253e058b5ea035158904856e83ce41811a636e5
Steve Degermark <sofacles@yahoo.com>
2022-12-26 07:48:13 -0800

    add an offset test for useOffsetMountainData

--- src/pretender/hooks/test-helpers/SimpleConsumerOfOffsetMountainDataProvider.tsx
+++ src/pretender/hooks/test-helpers/SimpleConsumerOfOffsetMountainDataProvider.tsx
@@ -7,10 +7,16 @@ import {
 
 const ComponentThatUsesHook = () => {
     const { state, dispatch } = useContext(OffsetMountainDataContext);
-
+const moveShipRight = () => {
+    
+    dispatch({type: 'UPDATE_GAME_OFFSET', cargo: {offsetDifference: -10}})
+}
     // console.info(state);
     return(
     <div>
+        <div>
+            <button data-testid="moveRightBtn" onClick={moveShipRight}>go right</button>
+        </div>
         test component <span>offset: {state.gameOffset}</span>
     </div>
     ); 
--- src/pretender/hooks/useOffsetMountainData.test.jsx
+++ src/pretender/hooks/useOffsetMountainData.test.jsx
@@ -1,7 +1,8 @@
 import React, { useContext } from "react";
-import { render, screen } from "@testing-library/react";
+import { getByTestId, fireEvent, render, screen } from "@testing-library/react";
 import { SimpleConsumerOfOffsetMountainDataProvider } from "./test-helpers/SimpleConsumerOfOffsetMountainDataProvider";
 
+console.log("The top of the test file");
 const renderTestContainer = () => {
   render(<SimpleConsumerOfOffsetMountainDataProvider />);
 };
@@ -14,6 +15,19 @@ describe("renders with the correct initial state", () => {
 
   it("has zero initial offset", () => {
     const linkElement = screen.getByText(/offset: 0/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+});
+
+describe("when UPDATE_GAME_OFFSET is dispatched with a negative number", () => {
+  beforeEach(() => {
+    renderTestContainer();
+    //fireEvent.click(screen.getByText("go right"));
+    fireEvent.click(screen.getByTestId("moveRightBtn"));
+  });
+
+  it("has a negative offset", () => {
+    const linkElement = screen.getByText(/offset: -10/i);
     expect(linkElement).toBeInTheDocument();
   });
 });
