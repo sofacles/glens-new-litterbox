05be7800b21f52ecf14cf618d9d937a2abf63a24
Steve Degermark <sofacles@yahoo.com>
2022-12-29 17:42:36 -0800

    the ship scrolls to the right forever

--- src/pretender/MainScreen.jsx
+++ src/pretender/MainScreen.jsx
@@ -9,6 +9,49 @@ import { OffsetMountainDataContext } from "./hooks/useOffsetMountainData";
 const MainScreen = () => {
   const { state, dispatch } = useContext(OffsetMountainDataContext);
   const { gameOffset, screenDimensions } = state;
+  const [observedTimeDeltas, setObservedTimeDeltas] = useState([]);
+  const [
+    timesAtWhichAnimationFrameWasCalled,
+    setTimesAtWhichAnimationFrameWasCalled,
+  ] = useState([]);
+  const PX_PER_SECOND = 200;
+
+  // Use useRef for mutable variables that we want to persist
+  // without triggering a re-render on their change
+  // https://css-tricks.com/using-requestanimationframe-with-react-hooks/
+  const requestRef = React.useRef();
+  const previousTimeRef = React.useRef();
+
+  const animate = (time) => {
+    setTimesAtWhichAnimationFrameWasCalled((oldArrTies) => [
+      ...oldArrTies,
+      time,
+    ]);
+
+    if (previousTimeRef.current != undefined) {
+      const deltaTime = time - previousTimeRef.current;
+
+      // Pass on a function to the setter of the state
+      // to make sure we always have the latest state
+      // setCount(prevCount => (prevCount + deltaTime * 0.01) % 100);
+      // OK, my use case is I'm trying to figure out how much to scroll the mountains.  So, let's say 200 px a second?
+
+      dispatch({
+        type: "UPDATE_GAME_OFFSET",
+        cargo: {
+          offsetDifference: Math.floor((deltaTime * PX_PER_SECOND) / 1000),
+        },
+      });
+    }
+    previousTimeRef.current = time;
+    requestRef.current = requestAnimationFrame(animate);
+  };
+
+  React.useEffect(() => {
+    requestRef.current = requestAnimationFrame(animate);
+    return () => cancelAnimationFrame(requestRef.current);
+  }, []); // Make sure the effect runs only once
+
   return (
     <>
       <InstrumentPanel gameOffset={gameOffset} />
