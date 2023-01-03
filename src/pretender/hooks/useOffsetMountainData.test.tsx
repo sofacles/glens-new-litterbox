8d606244f5f4b2a250665098d7c14cbfc88e2186
Steve Degermark <sofacles@yahoo.com>
2023-01-02 16:47:43 -0800

    fix test

--- src/pretender/hooks/useOffsetMountainData.test.tsx
+++ src/pretender/hooks/useOffsetMountainData.test.tsx
@@ -47,7 +47,6 @@ describe("when UPDATE_GAME_OFFSET is dispatched with a positive number", () => {
 describe("when UPDATE_GAME_OFFSET is dispatched with a negative number", () => {
   beforeEach(() => {
     renderTestContainer();
-    //
     for (let i = 0; i < 5; i++) {
       fireEvent.click(screen.getByText("move mountains left"));
     }
@@ -83,24 +82,29 @@ describe("when UPDATE_GAME_OFFSET is dispatched with a number greater than PANEL
 });
 
 //flying to the left, mountains are scrolling to the right, offset is decreasing below zero.
-describe("when UPDATE_GAME_OFFSET is dispatched with a negative number whose abs value is greater than PANEL_WIDTH", () => {
-  beforeEach(() => {
+describe("when UPDATE_GAME_OFFSET is dispatched twice with a negative number whose abs value is greater than PANEL_WIDTH", () => {
+  beforeEach(async () => {
     renderTestContainer();
 
-    fireEvent.click(screen.getByText("move mountains 110 pixels right"));
+    await fireEvent.click(screen.getByText("move mountains 110 pixels right"));
+    await fireEvent.click(screen.getByText("move mountains 110 pixels right"));
   });
 
   it("has a gameOffset equal to the amount you passed to the reducer", () => {
-    const linkElement = screen.getByText(/offset: -110/i);
+    const linkElement = screen.getByText(/offset: -220/i);
     expect(linkElement).toBeInTheDocument();
   });
 
-  it("has moved the right most mountain peak to the left end of the array", () => {
-    //The peak that was all the way on the left had x:-2000.  It will have been moved to the right 110 pixels, so -2000 - (-110) = -1890.  The rightmost  {x: 2000, y: 0} peak should 
+  it("has moved the two right most mountain peak to the left end of the array", () => {
+    //The peak that was all the way on the left had x:-2000.  It will have been moved to the right 220 pixels, so -2000 - (-220) = -1780.  The rightmost  {x: 2000, y: 0} peak should 
     //have been tacked onto the left side of the adjusted points and have an x value equal to 100 less than -1890: -1990.
-    expect(screen.getByTestId("leftMostPixelX")).toHaveTextContent("-1990");
+// Then the new right most peak would be moved to the left side of the array.  It started out {x: 1900, y: 40}, should become {x: -1900 -(-220) , y: 40 + }
+ const newXForSecondTackOn = -1990 + 110 - 100;
+ const newYForSecondTackOn =  verticalOffset - 40;
+     
+    expect(screen.getByTestId("leftMostPixelX")).toHaveTextContent(newXForSecondTackOn.toString());
     expect(screen.getByTestId("leftMostPixelY")).toHaveTextContent(
-      verticalOffset.toString()
+      newYForSecondTackOn.toString()
     );
   });
 });
