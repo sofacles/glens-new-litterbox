e1a67df5070211b944c4c23dc4e087e4a2080b6c
Steve Degermark <sofacles@yahoo.com>
2023-03-07 22:45:40 -0800

    rename bank to spiesAndMocks

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import {AccountList} from "./bank/MockingDataService/MockingClassMethod";
-import {AnimalRace} from "./bank/MockingClassHierarchy/MockingBaseClassMethod";
+import {AccountList} from "./spiesAndMocks/MockingDataService/MockingClassMethod";
+import {AnimalRace} from "./spiesAndMocks/MockingClassHierarchy/MockingBaseClassMethod";
 
 import './App.css';
 
 function App() {
-debugger;
   return (
     <div className="App">
       <AnimalRace />
