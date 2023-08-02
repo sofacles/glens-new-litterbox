f27c69d14e6770e8aae6a7a9a12d4347f7624e99
Steve Degermark <sofacles@yahoo.com>
2023-08-02 15:52:03 -0700

    fix a bug in RemoveNthNode, handle empty list, add tests

--- src/studying/RemoveNthNode.js
+++ src/studying/RemoveNthNode.js
@@ -3,6 +3,7 @@ console.log("remove the nth node");
 class LinkedList {
   constructor(arrNodes) {
     this.head = {};
+    this.isEmpty = arrNodes.length === 0;
     let ptr = this.head;
 
     for (let n = 0; n < arrNodes.length; n++) {
@@ -18,12 +19,20 @@ class LinkedList {
 
   printList() {
     console.log("printing the linked list");
+    if (this.isEmpty) {
+      console.log("List is empty.");
+      return "";
+    }
+    let output = "";
     let ptr = this.head;
     console.log(ptr.value);
+    output += `${ptr.value}, `;
     while (ptr.next !== null) {
       ptr = ptr.next;
       console.log(ptr.value);
+      output += `${ptr.value}, `;
     }
+    return output;
   }
 
   //if nfe = 2, then remove 2 from 1,2,3
@@ -31,7 +40,10 @@ class LinkedList {
     if (nfe < 1) {
       return;
     }
-
+    if (this.isEmpty) {
+      console.error(`List is empty Erroring out.`);
+      return;
+    }
     //first figure out the count of nodes in the list
     let ptr = this.head;
     let listLength = 1;
@@ -49,13 +61,15 @@ class LinkedList {
 
     //figure out what node to start at - the node before the node you want to to remove. So length - n - 1
     ptr = this.head;
-    for (var i = 1; i < listLength - nfe; i++) {
+    let i = 1;
+    for (i; i < listLength - nfe; i++) {
       ptr = ptr.next;
     }
     //now pointer is pointing at the node just before what we want to remove
     //make that node's next point at the node after next
     //special case: ptr is at head, so we need to remove the head node
-    if (ptr === this.head) {
+    //but ptr might have been incremented once, which means we're at the second node
+    if (ptr === this.head && listLength === nfe) {
       this.head = ptr.next;
     } else {
       ptr.next = ptr.next?.next || null;
@@ -63,7 +77,4 @@ class LinkedList {
   }
 }
 
-const myLL = new LinkedList([1, 2, 3]);
-myLL.printList();
-myLL.removeNthFromLast(1);
-myLL.printList();
+export default LinkedList;
---
+++ src/studying/RemoveNthNode.test.js
@@ -0,0 +1,51 @@
+import LinkedList from "./RemoveNthNode";
+const foo = jest.spyOn(console, "error");
+
+describe("RemoveNthNode", () => {
+  let linkedListUnderTest;
+  describe(" and linked list does contain nodes", () => {
+    beforeEach(() => {
+      linkedListUnderTest = new LinkedList([1, 2, 3]);
+    });
+
+    test("when passed 1", () => {
+      linkedListUnderTest.removeNthFromLast(1);
+      expect(linkedListUnderTest.printList()).toEqual("1, 2, ");
+    });
+
+    test("when passed 2", () => {
+      linkedListUnderTest.removeNthFromLast(2);
+      expect(linkedListUnderTest.printList()).toEqual("1, 3, ");
+    });
+
+    test("when passed 3", () => {
+      linkedListUnderTest.removeNthFromLast(3);
+      expect(linkedListUnderTest.printList()).toEqual("2, 3, ");
+    });
+
+    test("when passed an index that's greater than the number of nodes", () => {
+      linkedListUnderTest.removeNthFromLast(4);
+      expect(foo).toHaveBeenCalledWith(
+        "there aren't even 4 nodes in the list.  Erroring out."
+      );
+      expect(linkedListUnderTest.printList()).toEqual("1, 2, 3, ");
+    });
+  });
+
+  describe(" and linked list is empty", () => {
+    beforeEach(() => {
+      linkedListUnderTest = new LinkedList([]);
+    });
+
+    test("when passed 0", () => {
+      linkedListUnderTest.removeNthFromLast(0);
+      expect(linkedListUnderTest.printList()).toEqual("");
+    });
+
+    test("when passed an index that's greater than the number of nodes", () => {
+      linkedListUnderTest.removeNthFromLast(1);
+      expect(foo).toHaveBeenCalledWith("List is empty Erroring out.");
+      expect(linkedListUnderTest.printList()).toEqual("");
+    });
+  });
+});
