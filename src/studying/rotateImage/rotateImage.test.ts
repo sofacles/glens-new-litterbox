201db8a592a743951dc77552c1d27525288166f6
Steve Degermark <sofacles@yahoo.com>
2023-09-06 13:43:29 -0700

    broken, let's see if a 2x2 works

--- src/studying/rotateImage/rotateImage.test.ts
+++ src/studying/rotateImage/rotateImage.test.ts
@@ -1,15 +1,15 @@
 import { rotateImage } from "./rotateImage";
 
 describe("rotateImage", () => {
-  let systemUnderTest = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-  ];
   let transformed: number[][] | undefined;
 
-  describe(" 3x3", () => {
+  xdescribe(" 3x3", () => {
     beforeEach(() => {
+      let systemUnderTest = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+      ];
       transformed = rotateImage(systemUnderTest);
     });
     it("needs an it", () => {
@@ -17,6 +17,26 @@ describe("rotateImage", () => {
         [7, 4, 1],
         [8, 5, 2],
         [9, 6, 3],
+      ]);
+    });
+  });
+
+  describe(" 4 x 4", () => {
+    beforeEach(() => {
+      let systemUnderTest = [
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9, 10, 11, 12],
+        [13, 14, 15, 16],
+      ];
+      transformed = rotateImage(systemUnderTest);
+    });
+    it("needs an it", () => {
+      expect(transformed).toEqual([
+        [13, 9, 5, 1],
+        [14, 10, 6, 2],
+        [15, 11, 7, 3],
+        [16, 12, 8, 4],
       ]);
     });
   });
