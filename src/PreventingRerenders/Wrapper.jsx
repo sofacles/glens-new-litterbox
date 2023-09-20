b7e6d78676b31917151b855efe9b7d523ff6033f
Steve Degermark <sofacles@yahoo.com>
2023-09-20 16:08:51 -0700

    Putting the DeliveryDriver / Order stuff into its own folder

--- src/PreventingRerenders/Wrapper.jsx
+++ src/PreventingRerenders/Wrapper.jsx
@@ -1,20 +1,20 @@
 // This is the outermost component in this folder.  I am adding it so I can remember the trick of avoiding rerenders by putting components that really
 // shouldn't rerender (because the state that's being updated has nothing to do with their props) in as {children}
 
-import MainContainer from "./MainContainer";
-import { UnrelatedProps } from "./UnrelatedProps";
+import MainContainer from "./ComponentsInTheChildrenCollectionDoNotGetRerendered/MainContainer";
+import { UnrelatedProps } from "./ComponentsInTheChildrenCollectionDoNotGetRerendered/UnrelatedProps";
 import BasicRerenderingContainer from "./UseStateCausesRerenders/BasicRerenderingContainer";
 
 const Wrapper = () => {
   const foo = 4;
   return (
     <>
-      {/* <MainContainer>
+      <MainContainer>
         <UnrelatedProps count={foo} className="childOfApp">
           child of App does NOT get rerendered
         </UnrelatedProps>
-      </MainContainer> */}
-      <BasicRerenderingContainer />
+      </MainContainer>
+      {/* <BasicRerenderingContainer /> */}
     </>
   );
 };
