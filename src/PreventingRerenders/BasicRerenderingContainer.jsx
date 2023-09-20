88e21183092c4bea72286cefd179e4cdbcc4506a
Steve Degermark <sofacles@yahoo.com>
2023-09-20 13:00:29 -0700

    install SASS, clean up Rerendering
    about to prevent Order from rerendering if its props haven't changed

--- src/PreventingRerenders/BasicRerenderingContainer.jsx
+++ src/PreventingRerenders/BasicRerenderingContainer.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import StateVarUpdate from "./StateVarUpdate";
 import NonState from "./NonStateVarDoesNotUpdate";
 
+// OK, StateVarUpdate has a button in it that will update its own state variable, thus causing it to rerender itself.  But it does not result in
+// a rerender of this containing component.
+
 const BasicRerenderingContainer = () => {
   return (
     <div>
--- src/PreventingRerenders/NonStateVarDoesNotUpdate.jsx
+++ src/PreventingRerenders/NonStateVarDoesNotUpdate.jsx
@@ -1,18 +1,36 @@
+import React, { useEffect } from "react";
+
+import "./BasicRerenderingStyle.scss";
+
 const NonState = () => {
   let x = "x";
+
+  let x1 = "x1";
+  useEffect(() => {
+    x1 = "y1";
+  }, []);
+
   return (
-    <div>
-      Unless you use some way of maintaing state, updating a variable will not
-      rerender your component. value: {x}
-      <p>
-        <input
-          type="button"
-          value="update x"
-          onClick={() => {
-            x = "y";
-          }}
-        />{" "}
-      </p>
+    <div className="basic-rerenderer">
+      <div>
+        Unless you use some way of maintaining state, updating a variable will
+        not rerender your component. value: {x}
+        <p>
+          <input
+            type="button"
+            value="try to update x"
+            onClick={() => {
+              x = "y";
+            }}
+          />{" "}
+        </p>
+      </div>
+      <div>
+        <p>
+          How about if useEffect sets the variable? Has x1 been updated to "y1"?
+        </p>
+        x1: <span style={{ color: "red" }}>{x1}</span> No.
+      </div>
     </div>
   );
 };
